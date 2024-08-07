c8ef4cd0f27066af575015572e76fb27c1c33db3
Thames <chiratt.thames@gmail.com>
2024-08-08 00:29:49 +0700

    Update directory

--- src/routes/directory/+page.svelte
+++ src/routes/directory/+page.svelte
@@ -3,4 +3,26 @@
 </script>
 
 <Navbar />
-<h1>Directory</h1>
+<!-- Todo remove <br />-->
+<br />
+<div id="professor">
+	<img alt="Stack of Paper" />
+	<h1>Professor Directory</h1>
+	<p>find your professor through our google sheet</p>
+	<a href="/directory">Click here to view</a>
+</div>
+<br />
+<div id="staff">
+	<h1>Staff Directory</h1>
+	<button>Find the staff according to your topic</button>
+	<img alt="Magnifying Glass" />
+</div>
+
+<hr />
+
+<div id="info">
+	<p>Info ...</p>
+	<p>Info ...</p>
+	<p>Info ...</p>
+	<p>Info ...</p>
+</div>
