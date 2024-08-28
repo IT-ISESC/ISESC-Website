205afc3bd67ee787692c72c842643311cb5262c2
Thames <chiratt.thames@gmail.com>
2024-08-28 23:22:24 +0700

    Create staffData.js

---
+++ src/routes/testing/staffData.js
@@ -0,0 +1,44 @@
+// {
+//     nickname: '',
+//     fullname: '',
+//     tag: '',
+//     role: ''
+// },
+
+export const staffData = [
+	{
+		nickname: 'Sorn',
+		fullname: 'Suputtra Dachkun',
+		tag: 'Academic Affairs',
+		tagColor: '#84140e',
+		role: 'Head of Academics'
+	},
+	{
+		nickname: 'Prinze',
+		fullname: 'Sittipun Wunnasaen',
+		tag: 'Academic Affairs',
+		tagColor: '#84140e',
+		role: 'Registration Officer'
+	},
+	{
+		nickname: 'Mik',
+		fullname: 'Thanaphon Amattayakul',
+		tag: 'Academic Affairs',
+		tagColor: '#84140e',
+		role: 'Academic Officer'
+	},
+	{
+		nickname: 'Poon',
+		fullname: 'Punsita Bhuddhakulsomsri',
+		tag: 'Academic Affairs',
+		tagColor: '#84140e',
+		role: 'Academic Officer'
+	},
+	{
+		nickname: 'Ploy',
+		fullname: 'Panyata Sukyai',
+		tag: 'Academic Affairs',
+		tagColor: '#84140e',
+		role: 'Academic Officer'
+	}
+];
